cc8a5a670d11b09c71350aa001123614b99ba3c7
Harrison <90717856+Jhegerton@users.noreply.github.com>
2022-06-05 08:36:29 -0400

    Final commit for longest substring

--- LEET/Java/Leet-Exercise/src/LongestSubstring.java
+++ LEET/Java/Leet-Exercise/src/LongestSubstring.java
@@ -1,5 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
 public class LongestSubstring {
-    public static void run(){
-        System.out.println("Answer goes here");
+    public static void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder build = new StringBuilder();
+        Vector<Character> charVec = new Vector<>();
+        Vector<String> stringVec = new Vector<>();
+        int count = 0;
+        String dummy = "";
+
+        System.out.println("Enter a string");
+        String input = reader.readLine();
+
+        char[] arr = input.toCharArray();
+
+        for(char c : arr){
+            if (charVec.contains(c)) {
+                stringVec.add(build.toString());
+                charVec.clear();
+                build = new StringBuilder();
+            }
+            build.append(c);
+            charVec.add(c);
+        }
+
+        for(String substring : stringVec){
+            if(count < substring.length()){
+                dummy = substring;
+                count = substring.length();
+            }
+        }
+
+        System.out.println("The longest substring is: " + dummy + ", with the length of " + count);
+
+
+
+
     }
 }
