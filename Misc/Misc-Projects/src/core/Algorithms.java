1e47e90074623746fe2cb170ba542d37af7f4769
Harrison <90717856+Jhegerton@users.noreply.github.com>
2022-06-30 18:57:31 -0400

    Finished pick one

--- Misc/Misc-Projects/src/core/Algorithms.java
+++ Misc/Misc-Projects/src/core/Algorithms.java
@@ -1,47 +1,67 @@
 package core;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.math.BigInteger;
+import java.time.Clock;
+import java.util.*;
 import java.security.SecureRandom;
-import java.util.Scanner;
 
 public class Algorithms {
     static Scanner input = new Scanner(System.in);
     static List<String> choices = new ArrayList<>();
-    static SecureRandom rand = new SecureRandom();
+    static Random rand = new Random();
+
+    static Clock time = Clock.systemUTC();
 
     private Algorithms(){}
 
     public static void pickOne(){
+        rand.setSeed(time.millis());
         System.out.println("Enter your choices");
-         do{
-                choices.add(input.nextLine());
-        }while(input.hasNextLine());
+        boolean check = false;
+        final BigInteger PRIME = BigInteger.probablePrime(12, rand);
+        String next = "";
+        System.out.println(PRIME);
+        while(!check){
+            next = input.nextLine();
+            if(next.length() != 0) {
+                choices.add(next);
+            }
+            else{
+                check = true;
+            }
+
+        }
          int count = choices.size();
-         List<Integer> chance = new ArrayList<>();
-         for(int i = 0; i < 103; i++){
-             chance.add(rand.nextInt() % count);
-         }
-         Collections.sort(chance);
-         int winner = 0;
-         int greatest = 0;
-         int number = 0;
-         int past = 0;
-         for(Integer index : chance){
-             if(index == past){
-                 number++;
+
+         Map<Integer, Double> results = new HashMap<>();
+         double total = 0.0;
+
+         for(Integer i = 0; i < PRIME.intValue(); i++){
+             int res = rand.nextInt(Integer.MAX_VALUE) % count;
+             if(results.containsKey(res)){
+                 results.replace(res, results.get(res) + 1);
+                 total = total + results.get(res);
              }
              else{
-                 if(number > greatest){
-                     winner = index;
-                     greatest = number;
-                 }
-                 number = 1;
+                 results.put(res, 1.0);
              }
-
          }
-         System.out.println("The choice is " + choices.get(winner) + " with " + greatest/103 + "% of the vote.");
+        Integer winner = 0;
+        Double temp = 0.0;
+        for(Map.Entry<Integer, Double> entry : results.entrySet()){
+            Integer key = entry.getKey();
+             if(temp < results.get(key)){
+                 winner = key;
+                 temp = results.get(key);
+             }
+         }
+
+        for(Map.Entry<Integer, Double> entry : results.entrySet()){
+            Integer key = entry.getKey();
+            System.out.println(choices.get(key) + " with " + Math.round(results.get(key)) + " votes (" + Math.round(results.get(key) / PRIME.doubleValue() * 100) + "%)");
+        }
+        System.out.println(choices.get(winner));
+
     }
 
 }
