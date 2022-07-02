6429cfcd76c046fdb223e5cd57ff930f6843c1c7
Harrison <90717856+Jhegerton@users.noreply.github.com>
2022-07-02 12:45:21 -0400

    Finished cpp choice program

--- Misc/Misc-Cpp-Projects/main.cpp
+++ Misc/Misc-Cpp-Projects/main.cpp
@@ -1,60 +1,77 @@
 #include <iostream>
-#include <vector>
-#include <ctime>
 #include <map>
+#include <random>
+#include <string.h>
 
 int nextPrime(int first);
+void arrayCopy(char** src, char** dest, int start, int finish);
 
 using namespace std;
 
-int main() {
-    vector<string> vec;
-    vector<string> :: iterator it;
-    srand((int) time(0));
-    map<int, double> results;
-    string choice;
-    printf("%s\n", "Enter your choices");
-    do {
-        getline(cin, choice);
-        if (choice.length() != 0) {
-            vec.push_back(choice);
-        }
-    }while(choice.length() != 0);
-
-    int count = vec.size();
-
-
-    for(int i = 0; i < nextPrime(rand()); i++){
-        int res = rand() % count;
-        if(results.count(res)){
-            results[res] = results[res] + 1;
-        }else{
-            results.insert({res, 1.0});
-        }
+int main(int argc, char *argv[]) {
+    if(argc == 1){
+        printf("%s\n", "This program requires at least two arguments, type -h for help.");
+        return 1;
     }
-    int winner = 0;
-    double temp = 0.0;
-    double total = 0.0;
-    for(auto const& values : results){
-        if(temp < values.second){
-            winner = values.first;
-            temp = values.second;
-        }
-        total += values.second;
+    else if(0 == strcmp(argv[1], "-h") || 0 == strcmp(argv[1], "--help")){
+        printf("%s\n", "This program will choose from the choices that are provided as arguments.");
+        printf("%s\n", "Enter i at the end to see a detailed description");
+        return 0;
     }
+    else if(argc == 2){
+        printf("%s\n", "This program requires at least two arguments, type -h for help.");
+        return 1;
+    }
+    else {
+        char *choices[argc - 1];
+        arrayCopy(argv, choices, 1, argc);
+        random_device rd;
+        uniform_int_distribution<int> dist(5500, 8000);
 
-    for(auto const& values : results){
-        string string_obj(vec[values.first]);
-        char* name = &string_obj[0];
-        printf("%s with %.0f votes (%.2f%%)\n", name , values.second, values.second / total * 100);
+        int count = argc - 1;
+        map<int, double> results;
+
+        int prime = nextPrime(dist(rd));
+        for (int i = 0; i < prime; i++) {
+            int res = dist(rd) % count;
+            if (results.count(res)) {
+                results[res] = results[res] + 1;
+            } else {
+                results.insert({res, 1.0});
+            }
+        }
+        int winner = 0;
+        double temp = 0.0;
+        double total = 0.0;
+        for (auto const &values: results) {
+            if (temp < values.second) {
+                winner = values.first;
+                temp = values.second;
+            }
+            total += values.second;
+        }
+        printf("%s\n", choices[winner]);
+        char input = ' ';
+        scanf("%c", &input);
+
+        if (input == 'i') {
+            for (auto const &values: results) {
+                printf("%s with %.0f votes (%.2f%%)\n", choices[values.first], values.second,
+                       values.second / total * 100);
+            }
+        }
+
+
+        return 0;
     }
-    cout << vec[winner] << endl;
-    return 0;
 }
 
 int nextPrime(int first){
     bool check;
     int temp;
+    if(first > 10000){
+        return 1117;
+    }
     for(int i = first; i < first + 1000; i++){
        for(int j = 2; j < i; j++){
            if(i % j != 0){
@@ -70,5 +87,10 @@ int nextPrime(int first){
            return temp;
        }
     }
-
+    return nextPrime(first + 1000);
+}
+void arrayCopy(char* src[], char* dest[], int start, int finish){
+    for(int i = start; i < finish; i++){
+        dest[i - start] = src[i];
+    }
 }
