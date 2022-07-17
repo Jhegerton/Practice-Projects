d86eaf49d9bc6d29c5259f550409299baa9751c9
Harrison <90717856+Jhegerton@users.noreply.github.com>
2022-07-17 13:11:25 -0400

    Finished CardParsertest class

--- BlueHalo/Business-Card/tests/application/CardParserTest.java
+++ BlueHalo/Business-Card/tests/application/CardParserTest.java
@@ -7,16 +7,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardParserTest {
+    String doc;
+    Contact contact;
+
+    CardParser parser;
 
     @BeforeEach
     void setUp() {
+        doc = "Chronos Sleep Science&Chief Reseacher&George Johnson&Boston, MA& +1(446) 986 -0947&george@chronsos.net";
+        contact = new Contact("George Johnson", "14469860947", "george@chronsos.net");
+        parser = new CardParser();
     }
 
     @AfterEach
     void tearDown() {
+        doc = null;
+        contact = null;
+        parser = null;
+        assertNull(doc);
+        assertNull(contact);
+        assertNull(parser);
     }
 
     @Test
     void getContactInfo() {
+        try{
+            assertTrue(contact.equals(parser.getContactInfo(doc)));
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
