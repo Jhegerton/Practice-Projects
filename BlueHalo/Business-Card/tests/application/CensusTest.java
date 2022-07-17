ea3269f0f969bd636fd28ffbe698bb220b5a77c8
Harrison <90717856+Jhegerton@users.noreply.github.com>
2022-07-17 13:24:18 -0400

    Finished testing

--- BlueHalo/Business-Card/tests/application/CensusTest.java
+++ BlueHalo/Business-Card/tests/application/CensusTest.java
@@ -4,19 +4,28 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static application.Census.checkNames;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CensusTest {
+    String commonName;
+    String rareName;
 
     @BeforeEach
     void setUp() {
+        commonName = "Daniels";
+        rareName = "Edjerton";
     }
 
     @AfterEach
     void tearDown() {
+        commonName = null;
+        rareName = null;
     }
 
     @Test
-    void checkNames() {
+    void testCheckNames() {
+        assertTrue(checkNames(commonName));
+        assertFalse(checkNames(rareName));
     }
 }
