4d5ff546650949a343bc010aa3f24c92313de193
atlas <atlas@localhost>
2025-08-26 15:22:19 -0400

    Created GeometryCalculator and Geometry classes.

---
+++ Java Refresh/eclipse-workspace/PracticeReview/src/objects/GeometryCalculator.java
@@ -0,0 +1,100 @@
+package objects;
+public class GeometryCalculator extends Geometry{
+	
+	private Float x;
+	private Float y;
+	private Float z;
+	private Float r;
+	
+	
+	public GeometryCalculator() {
+		super();
+	}
+	
+	public GeometryCalculator(Float x, Float y, Float z, Float r) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.r = r;
+		
+	}
+
+	@Override
+	public Float x() {
+		return this.x;
+	}
+
+	@Override
+	public void x(Float x) {
+		this.x = x;
+	}
+
+	@Override
+	public Float y() {
+		return this.y;
+	}
+
+	@Override
+	public void y(Float y) {
+		this.y = y;
+	}
+
+	@Override
+	public Float z() {
+		return this.z;
+	}
+
+	@Override
+	public void z(Float z) {
+		this.z = z;
+	}
+
+	@Override
+	public Float r() {
+		return this.r;
+	}
+
+	@Override
+	public void r(Float r) {
+		this.r = r;
+	}
+
+	@Override
+	public Float area() {
+		return this.x * this.y;
+	}
+
+	@Override
+	public Float perimeter() {
+		return 2 *(this.x + this.y);
+	}
+
+	@Override
+	public Float volume() {
+		return this.x * this.y * this.z;
+	}
+
+	@Override
+	public Float circumference() {
+		return 2 * PI * this.r;
+	}
+
+	@Override
+	public Float diameter() {
+		return 2 * this.r;
+	}
+
+	@Override
+	public Float cArea() {
+		Double doubleCArea = PI * Math.pow(Double.valueOf(this.r.toString()),2.0);
+		return Float.valueOf(doubleCArea.toString());
+	}
+
+	@Override
+	public Float cVolume() {
+		Double doubleCVolume = (4.0/3.0) * (PI * Math.pow(Double.valueOf(this.r.toString()),3.0));
+		return Float.valueOf(doubleCVolume.toString());
+	}
+
+}
