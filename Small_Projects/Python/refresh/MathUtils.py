74cdacf5bf14cd3ba4bd694c6016379e517f2808
Jhegerton <90717856+Jhegerton@users.noreply.github.com>
2025-09-03 19:06:58 -0400

    Updated math utils

--- Small_Projects/Python/refresh/MathUtils.py
+++ Small_Projects/Python/refresh/MathUtils.py
@@ -1,27 +1,54 @@
 from math import ceil, sqrt
-from typing import Self
-
 class MathUtils:
-    instance: Self = None
+    _instance: 'MathUtils' = None
 
-    def ____init__(self: Self):
-        pass
+    def __new__(cls) -> 'MathUtils':
+        if cls._instance is None:
+            cls._instance =  super().__new__(cls)
+        return cls._instance
+
+    @staticmethod
+    def is_prime(number) -> bool:
+        min_val = 2
+        max_val = ceil(sqrt(number)) + 1
+        total_range = range(min_val, max_val)
+
+        for num in total_range:
+            if number % num == 0:
+                return False
+        return True
 
     @classmethod
-    def get_instance(cls):
-        if cls.instance is None:
-            cls.instance = cls()
-        return cls.instance
+    def get_prime_factors(cls, number_to_factor):
+        factors : list = [] # List to store prime factors
+        min_val : int = 2
+        max_val : int = ceil(sqrt(number_to_factor)) + 1
+        total_range : range = range(min_val, max_val)
 
-    def get_prime_factors(self: Self, num: int):
-        factors = []
-        min = 2
-        max = ceil(sqrt(num))
-        ran = range(min,max)
-        print(ran, sep=" ")
+        if not cls.is_prime(number_to_factor):
+            for num in total_range:
+                if number_to_factor % num == 0:
+                    # Divide and Conquer method to find prime factors
+                    factors.append(num)
+                    factors.extend(cls.get_prime_factors(number_to_factor // num))
+                    # Return once we have found all factors
+                    return factors
+        # if the number is prime return as a list of one
+        return [number_to_factor]
+
+    def __repr__(self):
+        return f"<<class=MathUtils, id={id(self)}>>"
 
 def main():
-    u = MathUtils.get_instance()
+    u1 = MathUtils()
+    u2 = MathUtils()
+    u3 = MathUtils()
+
+    print(f"u1: {u1}")
+    print(f"u2: {u2}")
+    print(f"u3: {u3}")
+
+    # print(u1.get_prime_factors(24))  # Example usage
 
 if __name__ == "__main__":
     main()
