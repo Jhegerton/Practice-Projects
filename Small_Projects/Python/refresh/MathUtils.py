8b14bbfc8bdaf8e703525a61c17b10f889dfb097
Jhegerton <90717856+Jhegerton@users.noreply.github.com>
2025-09-03 19:15:11 -0400

    Made minor changes to math utils.

--- Small_Projects/Python/refresh/MathUtils.py
+++ Small_Projects/Python/refresh/MathUtils.py
@@ -1,5 +1,12 @@
-from math import ceil, sqrt
+__author__ = ['Harrison Egerton']
+__export__ = ['MathUtils']
+
+from math import ceil, sqrt # unpack math functions
+
 class MathUtils:
+    """
+    MathUtils class contains methods to help with math operations
+    """
     _instance: 'MathUtils' = None
 
     def __new__(cls) -> 'MathUtils':
@@ -9,6 +16,7 @@ class MathUtils:
 
     @staticmethod
     def is_prime(number) -> bool:
+        # Determine a range of possible divisors
         min_val = 2
         max_val = ceil(sqrt(number)) + 1
         total_range = range(min_val, max_val)
@@ -19,8 +27,9 @@ class MathUtils:
         return True
 
     @classmethod
-    def get_prime_factors(cls, number_to_factor):
+    def get_prime_factors(cls, number_to_factor) -> list[int]:
         factors : list = [] # List to store prime factors
+        # Determine a range of possible divisors
         min_val : int = 2
         max_val : int = ceil(sqrt(number_to_factor)) + 1
         total_range : range = range(min_val, max_val)
@@ -36,14 +45,19 @@ class MathUtils:
         # if the number is prime return as a list of one
         return [number_to_factor]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        # Return a standard representation of the object
         return f"<<class=MathUtils, id={id(self)}>>"
 
-def main():
+
+def main(): # Main function
+    """Main function"""
     u1 = MathUtils()
     u2 = MathUtils()
     u3 = MathUtils()
 
+    # Test for singleton equality
+    # u1 and u2 and u3 should be the same object
     print(f"u1: {u1}")
     print(f"u2: {u2}")
     print(f"u3: {u3}")
