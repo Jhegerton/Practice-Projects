32c47b774d7ae0407387331d0b284b19dc6a357f
Jhegerton <90717856+Jhegerton@users.noreply.github.com>
2025-09-04 07:56:44 -0400

    Made the get_prime_factors self contained, removed external method call.

--- Small_Projects/Python/refresh/MathUtils.py
+++ Small_Projects/Python/refresh/MathUtils.py
@@ -34,15 +34,17 @@ class MathUtils:
         max_val : int = ceil(sqrt(number_to_factor)) + 1
         total_range : range = range(min_val, max_val)
 
-        if not cls.is_prime(number_to_factor):
+        if number_to_factor == 2: # Handle special case for 2:
+           return [2] # Smallest prime number
+        else:
             for num in total_range:
                 if number_to_factor % num == 0:
                     # Divide and Conquer method to find prime factors
-                    factors.append(num)
+                    factors.extend(cls.get_prime_factors(num))
                     factors.extend(cls.get_prime_factors(number_to_factor // num))
                     # Return once we have found all factors
                     return factors
-        # if the number is prime return as a list of one
+        # If number is prime, return it as a list of itself
         return [number_to_factor]
 
     def __repr__(self) -> str:
@@ -53,16 +55,11 @@ class MathUtils:
 def main(): # Main function
     """Main function"""
     u1 = MathUtils()
-    u2 = MathUtils()
-    u3 = MathUtils()
 
     # Test for singleton equality
     # u1 and u2 and u3 should be the same object
-    print(f"u1: {u1}")
-    print(f"u2: {u2}")
-    print(f"u3: {u3}")
 
-    # print(u1.get_prime_factors(24))  # Example usage
+    print(u1.get_prime_factors(24))  # Example usage
 
 if __name__ == "__main__":
     main()
